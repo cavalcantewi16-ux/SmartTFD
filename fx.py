9a3ef6e7dca01eccb8e34afdcc44e1a2d7be19ff
cavalcantewi16-ux <cavalcantewi16@gmail.com>
2026-08-09 20:07:03 -0300

    fix: nome correto do icone maps

---
+++ fx.py
@@ -0,0 +1,6 @@
+﻿with open(r'app\gestor\rotas-do-dia\page.tsx', encoding='utf-8') as f:
+    src = f.read()
+src2 = src.replace('/icone-google-maps.webp', '/icone-google-maps.webp.png')
+with open(r'app\gestor\rotas-do-dia\page.tsx', 'w', encoding='utf-8') as f:
+    f.write(src2)
+print('OK')
