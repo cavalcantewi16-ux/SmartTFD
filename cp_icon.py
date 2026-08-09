debe11e47225de60732b7be6bf65c2395dc470e6
cavalcantewi16-ux <cavalcantewi16@gmail.com>
2026-08-09 19:42:02 -0300

    chore: remove scripts

--- cp_icon.py
+++
@@ -1,6 +0,0 @@
-﻿import base64, shutil
-# copiar do sandbox para o projeto
-src = r'C:\Users\willi\AppData\Roaming\Claude\local-agent-mode-sessions\95aa13ae-ba49-41c8-bf6d-6efa6a642c91\75e7893c-3ee7-41f2-8898-b3aded6d689d\local_a804ed8f-032c-42b0-81dc-5af5dd502f34\outputs\maps-icon-check.png'
-dst = r'C:\SmartTFD\public\maps-icon.png'
-shutil.copy(src, dst)
-print('imagem copiada')
--- fp2.py
+++
@@ -1,4 +0,0 @@
-﻿with open(r'app\gestor\rotas-do-dia\page.tsx', encoding='utf-8') as f:
-    src = f.read()
-idx = src.find('setLocModal({ru:rota._uid,pu:pac._uid,q:pac.localizacao')
-print(repr(src[idx+60:idx+200]))
