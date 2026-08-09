407761df34bd5cfcef7aca11f487eaa88c0db627
cavalcantewi16-ux <cavalcantewi16@gmail.com>
2026-08-09 18:35:29 -0300

    chore: remove scripts de debug

--- fp.py
+++
@@ -1,9 +0,0 @@
-﻿with open(r'app\gestor\rotas-do-dia\page.tsx', encoding='utf-8') as f:
-    src = f.read()
-# interface PacRota
-idx = src.find('PacRota')
-print(repr(src[idx:idx+250]))
-print()
-# onde nome aparece no card JSX
-idx2 = src.find('pac.nome')
-print(repr(src[idx2-50:idx2+200]))
