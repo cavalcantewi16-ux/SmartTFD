80e85b13e1afe0cd5430c8913efd45184dec3375
cavalcantewi16-ux <cavalcantewi16@gmail.com>
2026-08-09 19:42:51 -0300

    chore: remove scripts

--- fx.py
+++
@@ -1,6 +0,0 @@
-﻿with open(r'app\gestor\rotas-do-dia\page.tsx', encoding='utf-8') as f:
-    src = f.read()
-src2 = src.replace('/maps-icon.png', '/icone-google-maps.webp')
-with open(r'app\gestor\rotas-do-dia\page.tsx', 'w', encoding='utf-8') as f:
-    f.write(src2)
-print('OK' if '/maps-icon.png' not in src2 else 'AVISO')
