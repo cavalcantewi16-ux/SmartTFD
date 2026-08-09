d4f2165c33225f7053d1a57685fdda552d60ee07
cavalcantewi16-ux <cavalcantewi16@gmail.com>
2026-08-09 19:42:00 -0300

    feat: icone Google Maps nos botoes de localizacao

---
+++ fp2.py
@@ -0,0 +1,4 @@
+﻿with open(r'app\gestor\rotas-do-dia\page.tsx', encoding='utf-8') as f:
+    src = f.read()
+idx = src.find('setLocModal({ru:rota._uid,pu:pac._uid,q:pac.localizacao')
+print(repr(src[idx+60:idx+200]))
---
+++ fp3.py
@@ -0,0 +1,12 @@
+﻿with open(r'app\gestor\rotas-do-dia\page.tsx', encoding='utf-8') as f:
+    src = f.read()
+
+old = 'className="text-blue-500 hover:text-blue-700 text-base px-1" title="Buscar no mapa">&#128205;</button>'
+new = 'className="hover:opacity-80 px-1" title="Buscar no mapa"><img src="/maps-icon.png" className="w-5 h-5" alt="Maps"/></button>'
+
+if old in src:
+    with open(r'app\gestor\rotas-do-dia\page.tsx', 'w', encoding='utf-8') as f:
+        f.write(src.replace(old, new, 1))
+    print('OK')
+else:
+    print('AVISO')
