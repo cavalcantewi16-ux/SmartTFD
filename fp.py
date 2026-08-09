30d378118970b157635010cebdbc50bd264f9915
cavalcantewi16-ux <cavalcantewi16@gmail.com>
2026-08-09 19:22:06 -0300

    chore: remove scripts

--- fp.py
+++
@@ -1,6 +0,0 @@
-﻿with open(r'app\gestor\rotas-do-dia\page.tsx', encoding='utf-8') as f:
-    src = f.read()
-
-# ver anchor do input de endereco do paciente
-idx = src.find('pac.localizacao')
-print(repr(src[idx-200:idx+200]))
--- fp2.py
+++
@@ -1,4 +0,0 @@
-﻿with open(r'app\gestor\rotas-do-dia\page.tsx', encoding='utf-8') as f:
-    src = f.read()
-idx = src.find('value={pac.hospital_id}')
-print(repr(src[idx-80:idx+350]))
