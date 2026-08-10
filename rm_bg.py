e45d69c8789d54c44f9adda72b6c7e74565af861
cavalcantewi16-ux <cavalcantewi16@gmail.com>
2026-08-10 11:12:29 -0300

    chore: remove scripts

--- rm_bg.py
+++
@@ -1,7 +0,0 @@
-﻿from PIL import Image
-img = Image.open(r'public\icone-google-maps.webp.png').convert('RGBA')
-pixels = list(img.getdata())
-new = [(r,g,b,0) if r>220 and g>220 and b>220 else (r,g,b,a) for r,g,b,a in pixels]
-img.putdata(new)
-img.save(r'public\icone-google-maps.webp.png','PNG')
-print('OK')
